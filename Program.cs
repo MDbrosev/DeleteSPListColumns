db4e16efe99a0f61fe8b856e9cc5eeefc4a1d998
Yoseph Hasson <23007047+MDbrosev@users.noreply.github.com>
2019-04-05 07:11:45 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint;
 
-        
+namespace DeleteSPListColumns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SPSite spSite = new SPSite("http://mySPSite:9000"))
+            {
+                using (SPWeb spWeb = spSite.OpenWeb())
+                {
+
+                    SPList isoDocs = spWeb.Lists["List Name Goes Here"];
+                    SPField field = isoDocs.Fields[new Guid("Item GUID Goes Here")];
+                    Console.WriteLine("Field Name:" + field.InternalName);
+
+                    field.Hidden = false;
+                    field.ReadOnlyField = false;
+                    field.Update();
+                    isoDocs.Fields.Delete(field.InternalName);
+                    Console.WriteLine("Field deleted - Done!");
+                }
+        }
+            Console.WriteLine("Press any key...");
+            Console.Read();
+        }
+    }
+}
